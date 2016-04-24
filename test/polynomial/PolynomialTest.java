aadb25a7dc9f55092a223155d5aa0564c8b7e0f6
Hendrik Werner <hendrik.to@gmail.com>
2016-04-24 02:13:49 +0200

    Implement test for divide().

--- test/polynomial/PolynomialTest.java
+++ test/polynomial/PolynomialTest.java
@@ -103,11 +103,7 @@ public class PolynomialTest {
     @Test
     public void testDivide() {
         System.out.println("divide");
-        Polynomial b = null;
-        Polynomial instance = new Polynomial();
-        instance.divide(b);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // not implemented as per specification
     }
 
     /**
